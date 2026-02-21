020285cf00a74617ca59fd31467ef926324a251b
Sean Lees <sean@mozeik.com>
2026-02-21 20:16:04 +0000

    feat: improve banner

--- src/ui/banner.ts
+++ src/ui/banner.ts
@@ -3,12 +3,18 @@ import { gradientColors } from "./theme.js";
 
 const bannerGradient = gradient([...gradientColors.banner]);
 
-// TODO: Replace with hardcoded ASCII art
 const BANNER = `
-  __   __ ___  ___  ___  _  _
-  \\ \\ / /|_ _|/ __||   \\| \\| |
-   \\ V /  | || (__ | |) | .\` |
-    \\_/  |___|\\___||___/|_|\\_|
+
+           d8,                         
+          \`8P                          
+                                       
+?88   d8P  88b d8888b d8888b   88bd88b 
+d88  d8P'  88Pd8P' \`Pd8P' ?88  88P' ?8b
+?8b ,88'  d88 88b    88b  d88 d88   88P
+\`?888P'  d88' \`?888P'\`?8888P'd88'   88b
+                                       
+                                       
+                                       
 `;
 
 /**
