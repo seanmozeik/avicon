997fa0b96ca1ca68c9a848f5f2e1cd28e17abe0f
Sean Lees <sean@mozeik.com>
2026-02-21 22:44:52 +0000

    fix: rename banner

--- src/ui/banner.ts
+++ src/ui/banner.ts
@@ -4,18 +4,17 @@ import { gradientColors } from "./theme.js";
 const bannerGradient = gradient([...gradientColors.banner]);
 
 const BANNER = `
-
-           d8,                         
-          \`8P                          
-                                       
-?88   d8P  88b d8888b d8888b   88bd88b 
-d88  d8P'  88Pd8P' \`Pd8P' ?88  88P' ?8b
-?8b ,88'  d88 88b    88b  d88 d88   88P
-\`?888P'  d88' \`?888P'\`?8888P'd88'   88b
-                                       
-                                       
-                                       
-`;
+                     d8,                         
+                      \`8P                          
+                                                   
+   d888b8b  ?88   d8P  88b d8888b d8888b   88bd88b 
+  d8P' ?88  d88  d8P'  88Pd8P' \`Pd8P' ?88  88P' ?8b
+  88b  ,88b ?8b ,88'  d88 88b    88b  d88 d88   88P
+  \`?88P'\`88b\`?888P'  d88' \`?888P'\`?8888P'd88'   88b
+                                                   
+                                                   
+                                                   
+  `;
 
 /**
  * Display the ASCII art banner with gradient colors
